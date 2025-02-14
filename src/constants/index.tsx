fdd27a16ac4c4d49e4085ce46c8a5291d2cd55de
Лодыгин Кирилл Валерьевич <lodygin@vtb.ru>
2025-02-14 12:27:50 +0300

    commit

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -6,29 +6,37 @@ export const TABLE_COLUMNS = [
     title: 'Имя',
     dataIndex: 'fullName',
     key: 'fullName',
+    width: '25%',
     sorter: (a: User, b: User) => a.fullName.localeCompare(b.fullName),
+    render: (value: string) => <div style={{ width: '100%' }}>{value}</div>,
   },
   {
     title: 'Электронная почта',
     dataIndex: 'email',
     key: 'email',
+    width: '25%',
     sorter: (a: User, b: User) => a.email.localeCompare(b.email),
+    render: (value: string) => <div style={{ width: '100%' }}>{value}</div>,
   },
   {
     title: 'Дата регистрации',
     dataIndex: 'registrationDate',
     key: 'registrationDate',
+    width: '25%',
     sorter: (a: User, b: User) => new Date(a.registrationDate).getTime() - new Date(b.registrationDate).getTime(),
+    render: (value: string) => <div style={{ width: '100%' }}>{value}</div>,
   },
   {
     title: 'Статус',
     dataIndex: 'userStatus',
     key: 'userStatus',
+    width: '25%',
     filters: [
       { text: 'Активен', value: 'Активен' },
       { text: 'Неактивен', value: 'Неактивен' },
     ],
     onFilter: (value: boolean | Key, record: User) => record.userStatus === value,
+    render: (value: string) => <div style={{ width: '100%' }}>{value}</div>,
   },
 ]
 
